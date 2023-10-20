99e7ca1ff9948da81078f1b1577d64a40540c60d
Ezekiel <ezekieljamolin186@gmail.com>
2023-10-19 22:34:06 -0700

    questions added

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is your project's title?"
+},
+{ 
+    type: "input",
+    name: "description",
+    message: "Write your project's description:"
+}, 
+{   
+    type: "input",
+    name: "Table of contents",
+    message: "What are your README's table of contents?"
+},
+{
+    type: "input",
+    name: "Installation",
+    message: "How will other people install your project?"
+}, 
+{
+    type: "input",
+    name: "Usage",
+    message: "How will your project be used?"
+}, 
+{
+    type: "list",
+    name: "License",
+    message: "What is your project's license?",
+    choices: ["None","Apache License 2.0","GNU General Public License v3.0","MIT License",'BSD 2-Clause "Simplified" License','BSD 3-Clause "New" or "Revised" License',"Boost Software License 1.0","Creative Commons Zero v1.0 Universal","Eclipse Public License 2.0","GNU Affero General Public License v3.0","GNU General Public License v2.0","GNU Lesser General Public License v2.1","Mozilla Public License 2.0","The Unlicense"]
+}, 
+{
+    type: "input",
+    name: "Contributing",
+    message: "How can others contribute to the project?"
+}, 
+{
+    type: "input",
+    name: "Tests",
+    message: "What tests have you done with the project?"
+},
+{
+    type: "input",
+    name: "Github",
+    message: "What is your github username?",
+    validate: function (input) { 
+        var validUrl = "https://github.com/" + input
+    
+    fetch(validUrl)
+    .then((response) => {
+      if (response.ok) {
+        console.log(`${input} is valid.`);
+      } else {
+        console.error(`${input} is not a valid account`);
+      }
+    })
+}
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions);
+}
 
 // Function call to initialize app
 init();
